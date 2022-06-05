e3ecb06d4f360dc4ff4c7726cc5e090eb17a9450
Rafl27 <raflsouza27@gmail.com>
2022-06-05 16:46:38 -0300

    login feito

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-
-
-//import express from 'express';
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const PORT = 3000;
 const app = express();
+app.use(express.static('Front/'));
+app.use(express.json());
 require('dotenv/config');
-
 app.use(bodyParser.json());
-//this way, everytime there's any type of request, it will be converted to json
-//import routes
-const postsRoute = require('./routes/posts')
-
-//middleware
-app.use('/posts', postsRoute);
-
-//routes
-app.get('/', (req,res) => {
-    res.send('We are on the home page now');
+const rotaVillage = require('./routes/Village')
+app.use('/village', rotaVillage);
+app.get('/html', (req,res) => {
+    res.sendFile("index.html");
 })
-
-
-//connect to DB
-//here i'm using the .env file and the dotenv packge to hide my mongoose link
-mongoose.connect(process.env.DB_CONNECTION, () => console.log('connected to the DB'));
-
+const villageConnection = "mongodb+srv://village:vilarejo@cluster0.ztwp6.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(villageConnection, () => console.log('connected to the DB'));
 app.listen(PORT, () => console.log(`The server is running on http://localhost:${PORT}`));
